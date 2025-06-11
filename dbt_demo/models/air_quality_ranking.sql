9a6b5e24d2a50c0c4b3388a2145138414a8ca457
Oliver Crawford <142399311+ojc-orchestra@users.noreply.github.com>
2025-06-11 14:34:08 +0100

    Update air_quality_ranking.sql

--- dbt_demo/models/air_quality_ranking.sql
+++ dbt_demo/models/air_quality_ranking.sql
@@ -4,7 +4,7 @@ SELECT
     year, 
     CASE
         WHEN AVG(pm25_concentration) <= 10 AND AVG(pm10_concentration) <= 20 AND AVG(no2_concentration) <= 40 THEN 'Good'
-        WHEN AVG(pm25_concentration) > 10 AND AVG(pm10_concentration) > 20 AND AVG(no2_concentration) > 50 THEN 'Poor'
+        WHEN AVG(pm25_concentration) > 10 AND AVG(pm10_concentration) > 20 AND AVG(no2_concentration) > 40 THEN 'Poor'
         ELSE 'Moderate'
     END as air_quality_rating
 FROM 
